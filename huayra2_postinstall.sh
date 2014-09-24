ea3ac7c07c6873a79a7170f5e47ba462c12ec118
Diego Ezequiel Guillén <diegoezequielguillen@gmail.com>
2014-09-24 15:50:15 -0300

    Ahora mueve las carpetas para no borrar la información

--- huayra2_postinstall.sh
+++ huayra2_postinstall.sh
@@ -1,29 +1,179 @@
-#!/bin/sh
+#!/bin/bash
 echo AGREGANDO REPOSITORIO DE COMUNIDAD...
 cd ~
 wget -c comunidadhuayra.org/ich.deb
 sudo dpkg -i ich.deb
 rm ich.deb
-echo ACTUALIZANDO FUENTES...
-apt-get clean
-apt-get update
-echo INSTALANDO DRIVERS INTEL
-sudo apt-get install -y intel-classmate-drivers
-sudo apt-get install -y intel-classmate-function-keys
-sudo apt-get install -y intel-pvr-support
 echo MONTANDO DATOS...
-mkdir /media/DATOS
-mount /dev/sda4 /media/DATOS
-mount /dev/sdb4 /media/DATOS
-echo CREANDO ESTRUCTURA DE DIRECTORIO EN DATOS...
-mkdir /media/DATOS/Mis\ Cosas
-mkdir /media/DATOS/Mis\ Cosas/Descargas
-mkdir /media/DATOS/Mis\ Cosas/Mi\ música
-mkdir /media/DATOS/Mis\ Cosas/Mis\ documentos
-mkdir /media/DATOS/Mis\ Cosas/Mis\ imágenes
-mkdir /media/DATOS/Mis\ Cosas/Mis\ vídeos
-echo EJECUTANDO POST-INSTALL...
-sudo apt-get install -y huayra-postinstall-pci
+if [ ! -f /etc/fstab ]; then
+        zenity --info --text "No existe /etc/fstab"
+        exit 1
+fi
+
+if ! command -v blkid > /dev/null ; then
+        zenity --info --text "No existe blkid"
+        exit 1
+fi
+
+#chequear si hay una partición con label=datos y si no salir
+blkid -L DATOS >> /dev/null
+if [ ! $? -eq 0 ]; then
+        zenity --info --text "No hay una partición con label DATOS"
+        exit 1
+fi
+
+DATOS_DEV=`blkid -L DATOS | head -n 1`
+if [ $? -eq 0 ]; then
+
+    DATOS_UUID=`blkid $DATOS_DEV|awk '{print $3}' | cut -d '=' -f 2 | sed -e 's/"//g'`
+
+
+    grep "$DATOS_DEV" /etc/fstab > /dev/null
+    if [ ! $? -eq 0 ]; then
+            grep "$DATOS_UUID" /etc/fstab > /dev/null
+            if [ ! $? -eq 0 ]; then
+                grep "LABEL=DATOS" /etc/fstab > /dev/null
+                if [ ! $? -eq 0 ]; then
+                    echo "LABEL=DATOS /media/DATOS vfat defaults,umask=0,errors=remount-ro 0 1" >> /etc/fstab
+                    echo "Se agregó la partición de DATOS a /etc/fstab"
+                fi
+            fi
+
+    fi
+
+    if [ ! -f /media/DATOS ]; then
+            mkdir -p /media/DATOS
+            mount /media/DATOS
+            echo "Se ha montado la partición DATOS"
+    fi
+fi
+
+#
+if [ -f /etc/fstab ]; then
+  sed -i".backup" '/usb/d' /etc/fstab
+else
+  echo "No existe /etc/fstab"
+fi
+
+#
+if [ ! -e /media/DATOS ]; then
+        echo "La partición DATOS no existe o no está montada"
+        exit 0
+fi
+
+if [ ! -e "/media/DATOS/Mis Cosas" ]; then
+		mkdir "/media/DATOS/Mis Cosas" 
+        echo "Creando carpeta Mis Cosas"
+fi
+
+for i in Descargas Música Documentos Imágenes Videos Vídeos; do
+	case $i in
+		Descargas) 
+			if [ -e /home/alumno/$i ]; then
+				echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Descargas" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Descargas/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Descargas"
+				fi;             
+        	fi;
+		;;
+		Música)
+			if [ -e /home/alumno/$i ]; then
+                echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Mi música" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Mi música/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Mi música"
+				fi; 
+        	fi;
+		;;
+		Documentos)
+			if [ -e /home/alumno/$i ]; then
+                echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Mis documentos" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Mis documentos/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Mis documentos"
+				fi;
+        	fi;
+		;;
+		Imágenes)
+			if [ -e /home/alumno/$i ]; then
+                echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Mis imágenes" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Mis imágenes/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Mis imágenes"
+				fi;
+        	fi;
+		;;
+		Videos)
+			if [ -e /home/alumno/$i ]; then
+                echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Mis vídeos" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Mis vídeos/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Mis vídeos"
+				fi;
+        	fi;
+		;;
+		Vídeos)
+			if [ -e /home/alumno/$i ]; then
+                echo "Moviendo carpeta $i"
+				if [ -e "/media/DATOS/Mis Cosas/Mis vídeos" ]; then
+					mv -f /home/alumno/$i/* "/media/DATOS/Mis Cosas/Mis vídeos/"
+				else
+					mv -f /home/alumno/$i "/media/DATOS/Mis Cosas/Mis vídeos"
+				fi;
+        	fi;
+		;;
+	  esac
+done
+
+ln -s "/media/DATOS/Mis Cosas/Descargas/" /home/alumno/Descargas
+ln -s "/media/DATOS/Mis Cosas/Mi música/" /home/alumno/Música
+ln -s "/media/DATOS/Mis Cosas/Mis documentos/" /home/alumno/Documentos
+ln -s "/media/DATOS/Mis Cosas/Mis imágenes/" /home/alumno/Imágenes
+ln -s "/media/DATOS/Mis Cosas/Mis vídeos/" /home/alumno/Videos
+chown -R alumno:alumno /home/alumno/Descargas /home/alumno/Música /home/alumno/Documentos /home/alumno/Imágenes /home/alumno/Videos
+
+echo "Symlinks creados en la carpeta Home del alumno"
+#
+cp -r /usr/share/huayra-postinstall-pci/alumno/.config/* /home/alumno/.config
+cp -r /usr/share/huayra-postinstall-pci/alumno/.local/* /home/alumno/.local
+echo "Agregando elementos de menú al usuario alumno"
+cp -r /usr/share/huayra-postinstall-pci/alumno/.config/* /etc/skel/.config
+cp -r /usr/share/huayra-postinstall-pci/alumno/.local/* /etc/skel/.local
+echo "Agregando elementos de menú al skel de usuarios"
+#!/bin/bash
+
+if [ ! -f /etc/udev/rules.d/99-hide-disks.rules ]; then
+        touch /etc/udev/rules.d/99-hide-disks.rules
+fi
+
+grep 'sda2' /etc/udev/rules.d/99-hide-disks.rules > /dev/null
+if [ ! $? -eq 0 ]; then
+    echo 'KERNEL=="sda2", ENV{UDISKS_PRESENTATION_HIDE}=1"' >> /etc/udev/rules.d/99-hide-disks.rules
+fi
+
+grep 'sda5' /etc/udev/rules.d/99-hide-disks.rules > /dev/null
+if [ ! $? -eq 0 ]; then
+    echo 'KERNEL=="sda5", ENV{UDISKS_PRESENTATION_HIDE}=1"' >> /etc/udev/rules.d/99-hide-disks.rules
+fi
+
+echo "Escondiendo particiones de Windows y Recuperación"
+
+#
+update-flashplugin-nonfree --install
+
+echo "Instalando plugin de flash"
+#
+apt-get update
+apt-get install -y intel-classmate-drivers
+apt-get install -y intel-classmate-function-keys
+apt-get install -y intel-pvr-support
+#
 echo CREANDO ENLACE CDPEDIA A DATOS...
 ln -s /media/DATOS/cdpedia /usr/share/cdpedia 
 echo RESETENDO TDAgent...
